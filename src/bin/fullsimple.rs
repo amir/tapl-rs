3d6ddc497ddd75bb15ccfcf545bad8388e0317e3
Amir Saeid <amir@glgdgt.com>
2017-09-16 19:44:53 +0100

    Add a binary for fullsimple

---
+++ src/bin/fullsimple.rs
@@ -0,0 +1,22 @@
+extern crate tapl;
+
+use std::io;
+use std::io::Write;
+
+use tapl::tapl::fullsimple;
+
+fn main() {
+    let mut input = String::new();
+    let context = fullsimple::Context::new();
+    loop {
+        print!("> ");
+        io::stdout().flush().unwrap();
+        io::stdin().read_line(&mut input).expect("read_line error");
+        input.trim();
+        match fullsimple::repl(&input, context.clone()) {
+            Ok(s) => println!("{}", s),
+            Err(e) => println!("{:?}", e),
+        }
+        input.clear();
+    }
+}
